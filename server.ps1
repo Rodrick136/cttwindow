cf201266d38b4dbcd782819df33bbffbc6861c44
Rodrick136 <rodrickdusio+github@gmail.com>
2022-09-26 11:54:05 -0700

    added a liscence and readme

--- server.ps1
+++ server.ps1
@@ -5,7 +5,9 @@ if (Test-Path "/ctt_values.json") {
   $values = Get-Content -Path "/values.json"
 }
 else {
+  $values = (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/MainWindow.xaml")
 }
+$values = $values | ConvertTo-Json -AsHashtable
 
 Start-PodeServer {
   Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http
@@ -14,6 +16,6 @@ Start-PodeServer {
     Write-PodeViewResponse -Path 'index'
   }
   Add-PodeRoute -Method Get -Path '/api/json/default' -ContentType 'application/json' -ScriptBlock {
-    Write-PodeJsonResponse -Value @{}
+    Write-PodeJsonResponse -Value $values
   }
 }
